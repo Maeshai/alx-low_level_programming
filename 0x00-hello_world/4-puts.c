1f3e02672538123124e0df6cb03d85748ab6ec96
Chaimaa El Ghandori <elghandori.chaimaa@gmail.com>
2023-03-16 09:06:49 -0700

    Add Script that prints exactly Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *main - this is the main function
+ *Return: always 0
+ */
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
