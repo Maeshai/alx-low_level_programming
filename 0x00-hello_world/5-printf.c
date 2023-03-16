046e289aa1bb678a36e3936bcbb783b445efdc0d
Chaimaa El Ghandori <elghandori.chaimaa@gmail.com>
2023-03-16 09:21:23 -0700

    Add Script that prints exactly with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,7 @@
  *Return: always 0
  */
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
