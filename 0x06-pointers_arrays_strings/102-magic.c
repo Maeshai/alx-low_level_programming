f256af1b40623e66bc4c8064b4028392d9b2435d
Chaimaa El Ghandori <elghandori.chaimaa@gmail.com>
2023-04-03 15:09:15 -0700

    Write a function that fills memory with a constant byte

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,30 @@
-#include <stdio.h>
 
-int main(void)
+#include "main.h"
+
+/**
+ * print_triangle -  a function that prints a triangle, followed by a new line.
+ * @size: An input integer
+ * Return: Always 0
+ */
+void print_triangle(int size)
 {
-	int n;
-	int a[5];
-	int *p;
+	int i = 0, j, n = size - 1;
 
-	a[2] = 1024;
-	p = &n;
-	/*
-	* write your line of code here...
-	* Remember:
-	* - you are not allowed to use a
-	* - you are not allowed to modify p
-	* - only one statement
-	* - you are not allowed to code anything else than this line of code
-	*/
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+	if (size > 0)
+	{
+	for (; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+	{
+	if (j < n)
+		_putchar(' ');
+		else
+		_putchar('#');
+	}
+		n--;
+		_putchar('\n');
+	}
+	}
+	else
+	_putchar('\n');
 }
--- README.md
+++
@@ -1,1 +0,0 @@
-Using GCC to compile  simple programs and studying the compilation process' steps
