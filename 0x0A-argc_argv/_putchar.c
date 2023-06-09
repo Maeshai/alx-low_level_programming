77f3265d9e2027aa43da2dded168b6e1479ccc87
Chaimaa El Ghandori <elghandori.chaimaa@gmail.com>
2023-06-09 08:04:42 -0700

    Write a program that prints its name, followed by a new line

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c:Ther character to print
- *    *
- *     * Return: On success 1.
+ * _putchar - writes the character c to stdout
+ * @c:Ther character to print
+ *
+ *Return: On success 1.
  * On error, -1 is returned and errno is set appropriately.
  */
 int _putchar(char c)
