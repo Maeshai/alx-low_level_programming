cb51be0e4cd317ff0becd39b6d8209dd15df1dd0
Chaimaa El Ghandori <elghandori.chaimaa@gmail.com>
2023-06-09 11:47:24 -0700

    Write a program that adds positive numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from the string
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = _atoi(argv[1]);
+	b = _atoi(argv[2]);
+	printf("%d\n", a * b);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum, i, j;
+
+	sum = 0;
+	if (argc != 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; j < (int)strlen(argv[i]); j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
