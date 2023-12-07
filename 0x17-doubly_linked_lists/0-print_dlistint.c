adf96aa1005be191586ab547ec03745a0831467a
Chaimaa El Ghandori <elghandori.chaimaa@gmail.com>
2023-12-07 06:19:10 -0800

    Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to the head of the double list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t node_count = 0;
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	node_count++;
+	}
+	return node_count;
+}
