8f8025aa3e98002cb12b8c2903a5a8210aa29cdd
Chaimaa El Ghandori <elghandori.chaimaa@gmail.com>
2023-12-07 06:24:30 -0800

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t node_count = 0;
+
 	while (h != NULL)
 	{
 	printf("%d\n", h->n);
 	h = h->next;
+
 	node_count++;
 	}
-	return node_count;
+	return (node_count);
 }
